26f94572999d7298eaa10e93f9880dc6151ccff1
Bramdon Santiago <bramdsantiago@gmail.com>
2026-03-11 11:30:09 -0600

    Deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/react-phone-book">
+    {/* <BrowserRouter basename="/react-phone-book"> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </StrictMode>,
 )
